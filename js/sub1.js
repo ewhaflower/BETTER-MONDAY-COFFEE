8159c78d22b1ef8ea40c42db16c8b3127304485b
ewhaflower <162560847+ewhaflower@users.noreply.github.com>
2024-05-20 02:15:10 +0900

    Add files via upload

---
+++ js/sub1.js
@@ -0,0 +1,15 @@
+<script>
+  var swiper = new Swiper(".sub_box", {
+    effect: "cube",
+  grabCursor: true,
+  cubeEffect: {
+    shadow: true,
+  slideShadows: true,
+  shadowOffset: 20,
+  shadowScale: 0.94,
+  },
+  pagination: {
+    el: ".swiper-pagination",
+  },
+});
+</script>
